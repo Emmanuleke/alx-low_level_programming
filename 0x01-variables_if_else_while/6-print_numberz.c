d68d9686d0aa695c6b2ecc1bbc766a4546ee1125
emmanuleke <emmanuelmatogo58@gmail.com>
2022-09-10 12:23:35 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+*main -entry point
+*description: this code prints out alphabets in lowercase
+*Return: always 0 (success)
+*/
+
+int main(void)
+
+{
+int num;
+for (num = 0; num <= 10; num++)
+printf("num");
+return (0);
+}
