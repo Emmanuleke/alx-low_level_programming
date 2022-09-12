3ecd2505981fa3b1f990bbb52591f854498fca65
emmanuleke <emmanuelmatogo58@gmail.com>
2022-09-12 09:55:31 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*additional headers go here*/
+/**
+ *main-execution
+ *this program printsout oupput
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art\n");
+return (0);
+}
