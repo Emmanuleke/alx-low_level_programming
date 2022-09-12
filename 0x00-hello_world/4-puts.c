6b74566e3879ce8f7d3de81767d27b5e76a78190
emmanuleke <emmanuelmatogo58@gmail.com>
2022-09-12 10:19:41 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*additional headers go here*/
+/**
+ * *main-execute
+ *this program printsout output
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+char ch[] = "\"Programming is like building a multilingual puzzle";
+puts(ch);
+puts("\n");
+return (0);
+}
