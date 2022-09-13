268b2efb2f9ec2b743644633559e699bc0d76d71
emmanuleke <emmanuelmatogo58@gmail.com>
2022-09-13 10:17:17 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*additional header goes hear*/
+/**
+ * main - starts execution
+ * this coode prints _putchar
+ * Return: always zero
+ */
+int main(void)
+{
+printf("_putchar");
+return (0);
+}
