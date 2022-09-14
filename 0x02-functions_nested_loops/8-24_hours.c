ed6cb4c42dc51af0ff9e5923b409abbc82d5b3a2
emmanuleke <emmanuelmatogo58@gmail.com>
2022-09-14 12:51:44 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+# include "main.h"
+/**
+ * jack_bauer- this executes the code 
+ * this code prints the minutes
+ * Return: always zero 
+ */
+void jack_bauer(void)
+{
+int hour = 0;
+int min = 0;
+int minreminder;
+int hourreminder;
+while (hour < 23)
+{while (min < 59)
+mireminder = min % 10;
+hourreminder = hours % 10;
+_putchar(hour/ 10 + '0');
+_putchar(hourreminder + '0');
+_putchar(':');
+_putchar(min/ 10 + '0');
+_putchar(minreminder + '0');
+min++;
+_putchar('\n');
+}
+hour++;
+min = 0;
+}
