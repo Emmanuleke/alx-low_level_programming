dc0870a550f776cce6154cc2ac0d3ea6c947f15e
emmanuleke <emmanuelmatogo58@gmail.com>
2022-09-14 11:37:33 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+# include "main.h"
+/**
+ * _abs- executes code
+ *@c: holds the valie
+ * Return: always zero
+ */
+int _abs(int c);
+{
+if (c == 0 || c > 0)
+return (c);
+else
+return (c * -1);
+}
