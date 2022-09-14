031ce10101de0457a9209e48ea085a14eb6bd312
emmanuleke <emmanuelmatogo58@gmail.com>
2022-09-14 08:06:03 -0700

    int print_sign(int n)

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_sign- executes the code
+ * @n: caries the value to be tested
+ * Return:
+ * always 0
+ */
+int print_sign(int n)
+{
+if (n > 0)
+_putchar(+);
+return (1);
+else if (n = 0)
+_putchar(0);
+return (0);
+else
+_putchar(-);
+return (-1);
+}
