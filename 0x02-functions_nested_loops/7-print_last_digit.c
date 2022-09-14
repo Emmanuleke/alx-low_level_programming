cfaff5595791b43b567fd5325260aedcda39351d
emmanuleke <emmanuelmatogo58@gmail.com>
2022-09-14 12:06:11 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+# include "main.h"
+/**
+ * print_last_digit- executes the code
+ * @n:holds the value
+ * Return: value of last digit
+ */
+int print_last_digit(int n)
+{
+int lastdigit = n % 10
+_putchari('lastdigit');
+return (0);
+}
