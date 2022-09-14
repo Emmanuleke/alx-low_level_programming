236882a0c0da1d1e6b1addce8c9d35997a552192
emmanuleke <emmanuelmatogo58@gmail.com>
2022-09-13 22:20:48 -0700

    void print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main"
+/**
+ * print alphabet- function tho brint alpabet
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+char c;
+for(c = 'a'; a <= 'z'; c++)
+_putchar(c);
+_putchar(\n);
+return (0);
+}
