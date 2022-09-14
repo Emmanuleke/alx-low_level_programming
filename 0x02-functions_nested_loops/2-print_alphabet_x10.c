4811310602708a80641c2725e3da049fd09c0825
emmanuleke <emmanuelmatogo58@gmail.com>
2022-09-14 05:30:34 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * print_alphabet- function to priny allpabet x10
+ *print_alphabet_x10-function to priny allpabet x10
  * this code prints alphabet
  * Return: 0
  */
 void print_alphabet_x10(void)
 {
+int i;
 for (i = '0'; i <= '9'; i++)
 {
 char c;
