b8f812a33e7b1fe3935ca993e8ebc48fed9c9223
emmanuleke <emmanuelmatogo58@gmail.com>
2022-09-26 11:48:44 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - the execution
+ * @s: contains the adress of b
+ * @b: the variable to be replaced
+ * @n: number of bytes to be replaced
+ * return: always zero
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+while (n)
+{
+s[n -1] = b;
+n--;
+}
+return (s);
+}
