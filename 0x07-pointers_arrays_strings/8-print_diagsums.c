db72f841fcba2f9aa6eb9bc87e691cfb3d3d08a7
emmanuleke <emmanuelmatogo58@gmail.com>
2022-09-27 11:48:56 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - execution of the code
+ * @a: this is a pre defined variable
+ * @size: this is a pre defined variable
+ * Return: always zero
+ */
+void print_diagsums(int *a, int size)
+{
+int i, j, p, l = 0, r = 0;
+for (i = 0; i < size; i++)
+{
+p = (i * size) + i;
+l += *(a + p);
+}
+for (j = 0; j < size; j++)
+{
+p = (j * size) + (size - l - j);
+r += *(a + p);
+}
+printf("%i, %i\n", l, r);
+}
