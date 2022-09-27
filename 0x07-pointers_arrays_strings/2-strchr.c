906a66a15643a148bab9e4cd1b34524f4eafdc8a
emmanuleke <emmanuelmatogo58@gmail.com>
2022-09-26 19:55:10 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,9 +9,11 @@ char *_strchr(char *s, char c)
 {
 int j;
 for (j = 0; *(s + j); j++)
-if (*(s + j) == c)
+{
+	if (*(s + j) == c)
 return (s + j);
-if (*(s +  j) == c
+}
+	if (*(s +  j) == c
 return (s + j);
 return (0);
 }
