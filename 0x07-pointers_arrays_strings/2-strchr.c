01a97430137a769828a61b69a9291c743c3351d5
emmanuleke <emmanuelmatogo58@gmail.com>
2022-09-26 19:42:58 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strchr - the executio begins here
+ * @s:this is where the characters are copied
+ * @c: this are the characters to be copied
+ * Return: always zero
+ */
+char *_strchr(char *s, char c)
+{
+int j;
+for (j = 0; *(s + j); j++)
+if (*(s + j) == c)
+return (*(s + j));
+}
