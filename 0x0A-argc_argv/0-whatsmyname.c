c62301a6b55228fb48dec89a1e736855095df98d
emmanuleke <emmanuelmatogo58@gmail.com>
2022-10-05 09:44:59 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,9 +3,10 @@
  * main - this prints its name
  * @argc: parameter
  * @argv: parameter
- * Return: 0 for success 
+ * Return: 0 for success
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 printf("%s\n", *argv);
+return (0);
 }
