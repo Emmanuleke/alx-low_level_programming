e2410be1940c011b23e0156a06c59c92c0b1f318
emmanuleke <emmanuelmatogo58@gmail.com>
2022-10-05 09:38:16 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this prints its name
+ * @argc: parameter
+ * @argv: parameter
+ * Return: 0 for success 
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", *argv);
+}
