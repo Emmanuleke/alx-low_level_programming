20d2c09278ba7bb5f5bd06d7d047cbff1049e0c6
emmanuleke <emmanuelmatogo58@gmail.com>
2022-10-11 11:13:05 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ *  init_dog functon prototype
+ *  struct dog - struvture data type
+ *  @d: pointer to structure
+ *  @name: name of owner
+ *  @age: age of owner
+ *  @owner:the owner
+ *  Return: 0 for success
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
+d.name = bosco;
+d.age = 20;
+d.owner = emmanuel;
+
+}
