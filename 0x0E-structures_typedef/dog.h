58f445aaa4f3c3503783dac29206d19f7b582606
emmanuleke <emmanuelmatogo58@gmail.com>
2022-10-11 09:36:45 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,10 +7,15 @@
  * @age: contains variable for age
  * @owner: the owner
  */
-struct dog my_dog                                                                                  {                                                                                  
-my_dog char *name;
-my_dog float age;
-my_dog char *owner;
+struct dog
+{                                                                                  
+char *name;
+float age;
+char *owner;
 };
-
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
