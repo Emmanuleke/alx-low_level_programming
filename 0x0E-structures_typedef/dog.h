7814052922f1c36f229580a3f659525afb2b037f
emmanuleke <emmanuelmatogo58@gmail.com>
2022-10-11 05:15:00 -0700

    dog,h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * main - the main function
+ * struct dog: a structure within main
+ * @name: first element
+ * @age: contains variable for age
+ * @owner: the owner
+ */
+struct dog my_dog                                                                                  {                                                                                  
+my_dog char *name;
+my_dog float age;
+my_dog char *owner;
+};
+
+#endif
